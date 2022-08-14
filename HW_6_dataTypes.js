f9eca2e0e10bb26d8514dd0792272f994d44ff71
Artem <artem.kas.cfc@gmail.com>
2022-08-14 17:16:28 +0300

    HW_3-10 are done

---
+++ HW_6_dataTypes.js
@@ -0,0 +1,30 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+const newString = 'Hello',
+      newNumber = 9,
+      newBoolean = true,
+      justNull = null,
+      justUndefined = undefined,
+      newObj = {},
+      newArray = [];
+
+console.log(typeof newString,
+            typeof newNumber,
+            typeof newBoolean,
+            typeof justNull,
+            typeof justUndefined,
+            typeof newObj,
+            typeof newArray);
